b41a4ee0c455004b0a79c27bbdaff92196b9ede9
victorviccente <victorvicente001@hotmail.com>
2025-05-19 17:46:10 -0300

    lint error 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import NDARoom from './pages/NdaRoom'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
